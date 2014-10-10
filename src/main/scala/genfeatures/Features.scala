a6ad1b1b9b8bbb44ff04c3cfcae13dbbeb134541
David Winslow <dwinslow@boundlessgeo.com>
2014-10-10 14:57:02 -0400

    Clean up a bit

--- src/main/scala/genfeatures/Features.scala
+++ src/main/scala/genfeatures/Features.scala
@@ -1,4 +1,5 @@
 import org.scalacheck.{ Arbitrary, Gen }
+import scala.collection.JavaConverters._
 
 package genfeatures {
   case class Column[A](name: String, gen: Gen[A], binding: Class[A])
@@ -23,11 +24,26 @@ package genfeatures {
       builder.buildFeatureType
     }
   }
+
+  sealed trait GeometryGenerators {
+    def fac: com.vividsolutions.jts.geom.GeometryFactory
+    implicit val arbPoint = Arbitrary {
+      for {
+        x <- Gen.chooseNum(-180d, 180d)
+        y <- Gen.chooseNum(-180d, 180d)
+      } yield fac.createPoint(new com.vividsolutions.jts.geom.Coordinate(x, y))
+    }
+  }
+
+  object GeometryGenerators {
+    def Default = new GeometryGenerators { def fac = new com.vividsolutions.jts.geom.GeometryFactory }
+  }
 }
 
 package object genfeatures {
   implicit class ColumnSugar(name: String) extends AnyRef {
     def as[A : Arbitrary : Manifest]: Column[A] = Column(name)
+    def from[A : Manifest](gen: Gen[A]): Column[A] = Column(name, gen, manifest[A].runtimeClass.asInstanceOf[Class[A]])
   }
 
   def sequence[A](gens: Seq[Gen[A]]): Gen[Seq[A]] = {
@@ -37,5 +53,34 @@ package object genfeatures {
         e <- elem
       } yield g :+ e
     }
+  }
+
+  def getShapefileStore(path: String): org.geotools.data.DataStore = {
+    val dsFactory = new org.geotools.data.shapefile.ShapefileDirectoryFactory
+    val params = Map[String, java.io.Serializable]("url" -> new java.net.URL("file:./")).asJava
+    dsFactory.createDataStore(params)
+  }
+
+  def generateNFeatures(store: org.geotools.data.DataStore, schema: Schema, n: Int) {
+    try {
+      store.createSchema(schema.toFeatureType)
+      val tx = new org.geotools.data.DefaultTransaction
+      try {
+        val writer: org.geotools.data.FeatureWriter[org.opengis.feature.simple.SimpleFeatureType, org.opengis.feature.simple.SimpleFeature]
+          = store.getFeatureWriter(schema.name, tx)
+        try {
+          val features = schema.features
+          for (_ <- 0 until n) {
+            val f = features.sample
+            val toWrite = writer.next
+            toWrite.setAttributes(f.get.getAttributes)
+            writer.write
+          } 
+        } finally writer.close()
+        tx.commit()
+      } catch {
+        case scala.util.control.NonFatal(e) => tx.rollback()
+      } finally tx.close()
+    } finally store.dispose()
   }
 }
--- src/main/scala/genfeatures/GenShapefile.scala
+++ src/main/scala/genfeatures/GenShapefile.scala
@@ -5,31 +5,11 @@ import org.scalacheck.{ Arbitrary, Gen }
 import org.scalacheck.Arbitrary.arbitrary
 
 object GenShapefile extends App {
-  val fac = new com.vividsolutions.jts.geom.GeometryFactory
-  implicit val arbPoint = Arbitrary {
-    for {
-      x <- Gen.chooseNum(-180d, 180d)
-      y <- Gen.chooseNum(-180d, 180d)
-    } yield fac.createPoint(new com.vividsolutions.jts.geom.Coordinate(x, y))
-  }
-
+  val geometries = GeometryGenerators.Default
+  import geometries._
   implicit val arbInteger = Arbitrary { arbitrary[Int].map(java.lang.Integer.valueOf _) }
-  val schema = Schema("test", Seq("the_geom".as[Point], "label".as[String], "value".as[java.lang.Integer]))
 
-  val dsFactory = new org.geotools.data.shapefile.ShapefileDataStoreFactory
-  val store = dsFactory.createDataStore(new java.net.URL("file:test.shp"))
-  store.createSchema(schema.toFeatureType)
-  val tx = new org.geotools.data.DefaultTransaction
-  val writer = store.getFeatureWriter("file:test", tx)
-  val features = schema.features
-  for (_ <- 0 until 100) {
-    val f = features.sample
-    val toWrite = writer.next
-    toWrite.setAttributes(f.get.getAttributes)
-    writer.write
-  } 
-  writer.close()
-  tx.commit()
-  tx.close()
-  store.dispose()
+  val schema = Schema("test", Seq("the_geom".as[Point], "label".as[String], "value".as[java.lang.Integer], "count".as[java.lang.Integer]))
+  val store = getShapefileStore("file:./")
+  generateNFeatures(store, schema, 1000)
 }
